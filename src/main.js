a19ca3cbec39768205ca873f196ad58338ce0cca
Replit user <>
2022-10-21 20:55:20 +0000

    Atualizando pasta lab

---
+++ src/main.js
@@ -0,0 +1,125 @@
+import IMask from "imask"
+import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg > g g:ntn-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg > g g:ntn-child(2) path")
+const ccLogo = document.querySelector(".cc-Logo > span:ntn-child(1) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercad: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type] [0])
+  ccBgColor02.setAttribute("fill", colors[type] [1])
+  ccLogo.setAttribute("src", 'cc-${type}.svg')
+}
+
+setCardType("mastercard")
+
+globalThis.setCardType = setCardType
+
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Data().getFullYear()).slice(2),
+      to: String(new Data().getFullYear() + 10).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+ const expirationDateMesKed = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+  {
+    mask: "0000 0000 0000 0000",
+    regex: /^4\d{0,15}/,
+    cardtype: "visa",
+  },
+    {
+    mask: "0000 0000 0000 0000",
+    regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+    cardtype: "mastercard",
+  },
+    {
+    mask: "0000 0000 0000 0000",
+    cardtype: "default",
+  },
+  ],
+  dispatch: function(appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMasked.compiledMaskes.find(function (item) {
+      return number.match(item.regex)
+    })
+
+    //console.log(foundMask)
+    return foundMask
+  },
+}
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  alert("Cartao adicionado!")
+})
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+
+  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value);
+})
+
+function updateSecurityCode(code){
+  const ccSecutiry = document.querySelector(".cc-security .value")
+
+  ccSecutiry.innerText = code.length === 0 ? "123" : code
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number){
+  const ccNumber = document.querySelector(".cc-number")
+
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+expirationDateMesKed.on("accept", () => {
+  updateExpirationDate(expirationDateMesKed.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value")
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
